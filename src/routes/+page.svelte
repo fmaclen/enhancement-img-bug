7802d781263606a55ced015b1727c4b1b3ae7053
Fernando Maclen <fmaclen@gmail.com>
2024-01-17 15:04:13 -0300

    fix paths

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
  import Logo from "$lib/Logo.svelte";
- import logoMiamiHerald from "$lib/assets/logo-miami-herald.png";
+ import logoMiamiHerald from "$lib/images/logo-miami-herald.png?enhanced";
 </script>
 
-<h1>enhancement:img bug</h1>
-
-<hr/>
+This one fails:
 
 <Logo name="The Miami Herald" src={logoMiamiHerald} />
+
+This one works:
+
+<!-- <enhanced:img class="logo" alt="The Miami Herald logo" src={logoMiamiHerald} /> -->
+
+<style>
+	.logo {
+		width: var(--size);
+		height: auto;
+	}
+</style>
